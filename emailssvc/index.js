f2fed9acfc33c251e39c4271b2b8e194385a19c9
akinwol <tobiakinwumi@gmail.com>
2021-04-11 10:35:09 -0400

    quick email ms with nodemailer

---
+++ emailssvc/index.js
@@ -0,0 +1,57 @@
+require('dotenv').config()
+const AWS = require('aws-sdk');
+const nodemailer = require("nodemailer")
+const { Consumer } = require('sqs-consumer');
+
+// Configure the region
+AWS.config.update({region: 'us-east-1'});
+
+const queueUrl = "https://sqs.us-east-1.amazonaws.com/399555142064/nodeshop.fifo";
+
+
+let transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: process.env.MAILTRAP_USER,
+      pass: process.env.MAILTRAP_PASS,
+    },
+});
+
+function sendMail(message) {
+    let sqsMessage = JSON.parse(message.Body);
+    const emailMessage = {
+        from: 'noreply@solfey.com',    // Sender address
+        to: sqsMessage.userEmail,     // Recipient address
+        subject: 'Order Received | NodeShop',    // Subject line
+        html: `<p>Hi ${sqsMessage.userEmail}.</p. <p>Your order of ${sqsMessage.itemsQuantity} ${sqsMessage.itemName} has been received and is being processed.</p> <p> Thank you for shopping with us! </p>` // Plain text body
+    };
+
+    transport.sendMail(emailMessage, (err, info) => {
+        if (err) {
+            console.log(`EmailsSvc | ERROR: ${err}`)
+        } else {
+            console.log(`EmailsSvc | INFO: ${info}`);
+        }
+    });
+}
+
+// Create our consumer
+const app = Consumer.create({
+    queueUrl: queueUrl,
+    handleMessage: async (message) => {
+        sendMail(message);
+    },
+    sqs: new AWS.SQS()
+});
+
+app.on('error', (err) => {
+    console.error(err.message);
+});
+
+app.on('processing_error', (err) => {
+    console.error(err.message);
+});
+
+console.log('Emails service is running');
+app.start();
